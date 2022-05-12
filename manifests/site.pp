ded52c52317992dc83def02152ff1149a9e7abc9
Enrymcost <105204266+Enrymcost@users.noreply.github.com>
2022-05-12 23:32:35 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 node default {
-  file { '/root/README':
-    ensure => file,
-    content =>  $fqdn,
-    owner   => 'root',
-  }
 }
 node 'master.puppet.vm' {
   include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content =>  "welcome to ($fqdn)",
+    owner   => 'root',
+  }
 }
 node /*web/ { 
    include role::app_server
